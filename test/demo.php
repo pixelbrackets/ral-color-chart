8ab63e3d23567648d0c84d9a3dd6811f973f927e
Dan Untenzu <mail@pixelbrackets.de>
2018-11-11 22:34:06 +0100

    [FEATURE] Demo: Add grouped list

--- test/demo.php
+++ test/demo.php
@@ -9,3 +9,12 @@ $colors = $colorChart->getList();
 foreach ($colors as $color) {
     echo 'Color: ' . $color['number'] . ' Index: ' . $color['rangeindex'] . ' Name: ' . $color['nameEnglish'] . PHP_EOL;
 }
+
+echo 'Grouped List' . PHP_EOL;
+$colorGroups = $colorChart->getGroupedList();
+foreach ($colorGroups as $colorIndex => $colorGroup) {
+    echo $colorIndex . PHP_EOL;
+    foreach ($colorGroup as $color) {
+        echo 'Color: ' . $color['number'] . ' Index: ' . $color['rangeindex'] . ' Name: ' . $color['nameEnglish'] . PHP_EOL;
+    }
+}
